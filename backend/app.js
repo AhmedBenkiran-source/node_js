099d4b4470c63054b83eeb543787e849f6f57862
ahmed <ahmedbenkiran@outlook.fr>
2020-09-15 13:13:50 +0200

    Optimisez la structure du back-end

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const Thing = require('./models/thing');
-
+const stuffRoutes = require('./routes/stuff');
 mongoose.connect('mongodb+srv://root:root@cluster0.4rgtu.mongodb.net/test?retryWrites=true&w=majority',
     { useNewUrlParser: true,
         useUnifiedTopology: true })
@@ -19,35 +19,6 @@ app.use((req, res, next) => {
 });
 app.use(bodyParser.json());
 
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    });
-    thing.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-        .catch(error => res.status(400).json({ error }));
-});
-app.get('/api/stuff', (req, res, next) => {
-    Thing.find()
-        .then(things => res.status(200).json(things))
-        .catch(error => res.status(400).json({ error }));
-});
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-        .then(thing => res.status(200).json(thing))
-        .catch(error => res.status(404).json({ error }));
-});
-app.put('/api/stuff/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-        .catch(error => res.status(400).json({ error }));
-});
-app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-        .catch(error => res.status(400).json({ error }));
-});
-
+app.use('/api/stuff', stuffRoutes);
 module.exports = app;
 //https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg
