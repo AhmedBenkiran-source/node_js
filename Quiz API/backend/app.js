643948e65f1ac026d5472a4eb88c07306af1c59a
ahmed <ahmedbenkiran@outlook.fr>
2020-09-15 12:26:21 +0200

    Créer une API REST avec Node, Express et MongoDB

--- Quiz API/backend/app.js
+++ Quiz API/backend/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const Thing = require('./models/thing');
+const Product = require('./models/product');
 
 mongoose.connect('mongodb+srv://root:root@cluster0.4rgtu.mongodb.net/test?retryWrites=true&w=majority',
     { useNewUrlParser: true,
@@ -19,35 +19,33 @@ app.use((req, res, next) => {
 });
 app.use(bodyParser.json());
 
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
+app.post('/api/products', (req, res, next) => {
+    const product  = new Product({
         ...req.body
     });
-    thing.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    product.save()
+        .then(product =>res.status(201).json({ product }))
         .catch(error => res.status(400).json({ error }));
 });
-app.get('/api/stuff', (req, res, next) => {
-    Thing.find()
-        .then(things => res.status(200).json(things))
+app.get('/api/products', (req, res, next) => {
+    Product.find()
+        .then(products => res.status(200).json({products}))
         .catch(error => res.status(400).json({ error }));
 });
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-        .then(thing => res.status(200).json(thing))
+app.get('/api/products/:id', (req, res, next) => {
+    Product.findOne({ _id: req.params.id })
+        .then(product => res.status(200).json({product}))
         .catch(error => res.status(404).json({ error }));
 });
-app.put('/api/stuff/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+app.put('/api/products/:id', (req, res, next) => {
+    Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Objet modifié !'}))
         .catch(error => res.status(400).json({ error }));
 });
-app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
+app.delete('/api/products/:id', (req, res, next) => {
+    Product.deleteOne({ _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
         .catch(error => res.status(400).json({ error }));
 });
-
 module.exports = app;
 //https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg
