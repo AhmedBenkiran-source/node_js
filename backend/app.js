a27a915736290f5ee7143f2a5cc41d1e87f3d6e2
ahmed <ahmedbenkiran@outlook.fr>
2020-09-15 11:25:42 +0200

    Configurez Mongo DB

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 
 const app = express();
+const mongoose = require('mongoose');
+const Thing = require('./models/thing');
+
+mongoose.connect('mongodb+srv://root:root@cluster0.4rgtu.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+        useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
-app.use(bodyParser.json());
-
-app.post('/api/stuff',(req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: 'Objet créé !! '
-    });
-})
 app.use('/api/stuff', (req, res, next) => {
     const stuff = [
         {
